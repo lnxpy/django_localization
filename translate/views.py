927275abcc58654c402ef6b390e24249555f940c
lnxpy <lnxpylnxpy@gmail.com>
2020-08-16 01:29:51 +0430

    simplest HomeView created

--- translate/views.py
+++ translate/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils.translation import gettext as _
 
-# Create your views here.
+def HomeView(request):
+    text = _('Hello World')
+    return HttpResponse(text)
