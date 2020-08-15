dd72725671dc593042b833d3fd2d69d31dc07d22
lnxpy <lnxpylnxpy@gmail.com>
2020-08-16 02:35:04 +0430

    project-level configs removed

--- django_localization/settings.py
+++ django_localization/settings.py
@@ -121,8 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Localization configurations
-LOCALE_PATHS = [
-    BASE_DIR / 'locale',
-]
